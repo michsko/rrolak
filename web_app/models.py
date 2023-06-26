20a59420dca0f9ea745dd23521869865615ee1d1
popik <popik@pop-os.localdomain>
2023-06-26 18:02:46 +0200

    db entries

--- web_app/models.py
+++ web_app/models.py
@@ -10,11 +10,12 @@ Klub = get_user_model()
 
 class Profil(models.Model):
 	klub = models.ForeignKey(Klub, on_delete=models.CASCADE)
-	is_klub = models.IntegerField()
+	id_klub = models.IntegerField()
 	logo_klub = models.ImageField(upload_to='klub_logo', default='csar_logo/logo_csar.jpeg')
 	info = models.TextField(blank=True)
 	adressa = models.CharField(max_length=255, blank=True)
-
+	email = models.CharField(max_length=255, blank=True)
+	telefon = models.CharField(max_length=255, blank=True)
 
 	def __str__ (self):
-		return self.klub.name
+		return self.klub.username
