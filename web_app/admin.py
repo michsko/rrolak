53f4987e76113e65e339636869748a04472d6509
popik <popik@pop-os.localdomain>
2023-06-26 15:22:59 +0200

    profil model

---
+++ web_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Profil
+# Register your models here.
+
+
+admin.site.register(Profil)
---
+++ web_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+Klub = get_user_model()
+
+
+# Create your models here.
+
+
+class Profil(models.Model):
+	klub = models.ForeignKey(Klub, on_delete=models.CASCADE)
+	is_klub = models.IntegerField()
+	logo_klub = models.ImageField(upload_to='klub_logo', default='csar_logo/logo_csar.jpeg')
+	info = models.TextField(blank=True)
+	adressa = models.CharField(max_length=255, blank=True)
+
+
+	def __str__ (self):
+		return self.klub.name
